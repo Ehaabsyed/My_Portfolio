8f9ce4e73d637dc23be50fec0d2a1be5a154676a
Ehaabsyed <syedehaab12@gmail.com>
2025-08-16 19:36:28 +0530

    Changes

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loader() {
     return (
-        <div className='h-screen w-full flex justify-center items-center bg-[#1a1527]'>
+        <div className='h-screen w-full flex justify-center items-center bg-[#100E17]'>
 
             <div class="loader">
                 <span class="loader-text">loading</span>
